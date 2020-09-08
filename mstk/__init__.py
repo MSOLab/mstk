6ce03761edc7680342d49d824529b5ef26e0411e
KalebMoon <35506238+KalebMoon@users.noreply.github.com>
2020-09-08 20:39:11 +0900

    resolved an import error

--- mstk/__init__.py
+++ mstk/__init__.py
@@ -1,13 +1,3 @@
-__all__ = [
-    "AcTypes",
-    "Interval",
-    "Activity",
-    "Operation",
-    "Machine",
-    "Schedule",
-    "Cmap",
-]
-
 from .schedule.ac_types import AcTypesParam
 from .schedule.interval import Interval
 from .schedule.activity import Activity, Operation
@@ -18,5 +8,3 @@ from .visualize.color_map import Cmap
 
 from .visualize.plot_schedule import PlotSchedule
 from .read_schedule import read_schedule
-
-# TODO: use AcTypes as a global param
